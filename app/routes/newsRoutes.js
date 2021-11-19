af2c06abb74e527501bf0cf8e96b306284640888
Ruvim <malyshev.dev@ya.ru>
2021-11-19 12:44:10 +0300

    api-nodejs-express-mysql - added swagger sitemap and added access token for requests

--- app/routes/newsRoutes.js
+++ app/routes/newsRoutes.js
@@ -1,8 +1,9 @@
 const ObjectID = require('mongodb').ObjectId;
+const {firewall,expressAccessToken} = require('../../config/access')
 
 module.exports = function(app,db) {
     //http://localhost:8000/news/_id_ - GET (singl post)
-    app.get('/news/:id', (req, res) => {
+    app.get('/news/:id', expressAccessToken, firewall,(req, res) => {
         const params = { '_id': new ObjectID(req.params.id) };
         db.collection('News').findOne(params, (err, result) => {
             if(err) {
@@ -14,7 +15,7 @@ module.exports = function(app,db) {
     });
 
     //http://localhost:8000/news/ - GET  (all posts)
-    app.get('/news/', (req, res) => {
+    app.get('/news/', expressAccessToken, firewall,(req, res) => {
         db.collection('News').find().toArray((err, result) => {
             if(err) {
                 res.send({'error': err})
@@ -25,7 +26,7 @@ module.exports = function(app,db) {
     });
 
     //http://localhost:8000/news/ - POST (insert post)
-    app.post('/news/',(req,res) => {
+    app.post('/news/', expressAccessToken, firewall,(req,res) => {
         const news = { text: req.body.body, title: req.body.title }
         db.collection('News').insert(news, (err, result) => {
             if(err) {
@@ -37,10 +38,10 @@ module.exports = function(app,db) {
     });
 
     //http://localhost:8000/news/id - DELETE (delete post)
-    app.delete('/news/:id',(req,res) => {
+    app.delete('/news/:id', expressAccessToken, firewall,(req,res) => {
         const id = req.params.id;
         const params = { '_id': new ObjectID(id) };
-       
+
         db.collection('News').remove(params, (err, result) => {
             if(err) {
                 res.send({'error': err})
@@ -50,8 +51,8 @@ module.exports = function(app,db) {
         })
     });
 
-    //http://localhost:8000/news/id - PUT (update post) 
-    app.put('/news/:id',(req,res) => {
+    //http://localhost:8000/news/id - PUT (update post)
+    app.put('/news/:id', expressAccessToken, firewall,(req,res) => {
         const id = req.params.id;
         const params = { '_id': new ObjectID(id) };
         const news = { text: req.body.body, title: req.body.title }
--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express')
 const MongoClient = require('mongodb').MongoClient
 const bodyParser = require('body-parser')
 const db = require('./config/db')
+const cookieParser = require('cookie-parser')
+
+const sitemap = require('express-sitemap-html')
 
 const app = express();
 const port = 8000;
 
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }))
 
 MongoClient.connect(db.url, (err, client) => {
@@ -14,6 +18,9 @@ MongoClient.connect(db.url, (err, client) => {
     const db = client.db('ruvemru');
 
     require('./app/routes')(app,db);
+
+    app.get('/sitemap', sitemap(app))
+    sitemap.swagger('My API', app)
 
     app.listen(port, () => {
         console.log('Server Api is UP on port '+ port)
